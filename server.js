bcf6663e1f63525b7fda5e17b98a761ce4d5f8bc
Mike Skelton <mskelton19@Mikes-MBP.home>
2017-08-18 16:29:02 -0400

    heroku set up

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
+
+mongoose.connect(mongoUri);
 
 app.listen(3000, ()=>{
 	console.log('listening');

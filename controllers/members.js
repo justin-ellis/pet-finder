e0d0f8f7f19f976d152a25dfd9ffd25fedf52b75
Mike Skelton <mskelton19@Mikes-MBP.home>
2017-08-20 18:00:18 -0400

    created routes for edit, new, index and show. more work to follow

--- controllers/members.js
+++ controllers/members.js
@@ -4,8 +4,29 @@ const Members = require('../models/members.js');
 
 router.get('/', (req, res)=>{
 	Members.find({}, (err, foundMembers)=>{
-		res.json(foundMembers);
+		res.render('members/index.ejs');
 	});
 });
+
+router.get('/new', (req, res) => {
+	res.render('members/new.ejs')
+})
+
+router.get('/:id', (req, res) => {
+	User.findById(req.params.id, (error, Member) => {
+		res.render('members/show.ejs')
+	})
+})
+
+router.get('/:id/edit', (req, res) => {
+  User.findById(req.params.id, (error, editMember) => {
+    res.render('members/edit.ejs', {
+    })
+  })
+})
+
+
+
+
 
 module.exports = router;

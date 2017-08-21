b11ee92433c23182a0f98f8025518272686ec870
Jellis <venetianself@hotmail.com>
2017-08-21 16:02:25 -0500

    Added seed controller, no data yet

--- controllers/pets.js
+++ controllers/pets.js
@@ -5,17 +5,20 @@ const getBreedList = require('../bin/petfinder.js')
 const getSheltersWithBreeds = require('../bin/petfinder.js')
 
 router.get('/getBreedList', (req, res)=>{
+	console.log('req.body: ', req.body);
+	getBreedList(res, req.body.zip,
+		req.body.animal);
 
-	getBreedList(res);
-
-	
-	// Pets.find({}, (err, foundUsers)=>{
+	// Pets.find({}, (err foundUsers)=>{
 	// 	res.json(foundPets);
 	// });
 });
 
 router.get('/getSheltersWithBreeds', (req, res)=>{
-	getSheltersWithBreeds(res);
+	console.log('req.body: ', req.body);
+	getSheltersWithBreeds(res, req.body.zip,
+		req.body.animal);
+		
 });
 
 module.exports = router;
--- controllers/session.js
+++ controllers/session.js
@@ -45,7 +45,7 @@ User.create(userDbEntry, (err, user)=>{
 	req.session.username = user.username;
 	req.session.logged = true;
 	});
-	res.redirect('/members');
+	res.redirect('/members/new.ejs');
 });
 
 // Member.create(req.body, (err, createdMember)=>{

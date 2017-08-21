11ab0a899924de181bc9de2df757ea298d683ac1
stefani spayde <spayde.stefani@gmail.com>
2017-08-21 13:50:00 -0500

    updating index.html with bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -16,14 +16,14 @@ app.controller('PetController', ['$http', function($http){
 		}
 	);
 };
-	this.getSheltersWithBreeds  = function(animal, breed, location) {
+	this.getSheltersWithBreeds  = function(animal, breed, shelterId) {
 		$http({
 			method: "GET",
 			url: "#",
 			data: {
 				name: this.name,
 				description: this.description,
-				contact.email: this.contact.email,
+				contact.email: this.contact.email, //data[0].contact.
 				contact.phone: this.contact.phone
 			}
 		}).then(function, (response){
